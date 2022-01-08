701d1d5287e04c7d524db4a480983262842f1233
Soroush <s_shemshadi@ymail.com>
2022-01-08 13:08:36 +0330

    INITIALIZE(spring boot app): initialize a maven spring boot app project files.
    Add pom.xml , application properties for SpringBoot app and Java main file for application.

---
+++ src/main/java/io/github/shuoros/reporeporter/RepoReporterApplication.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.reporeporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoReporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoReporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/shuoros/reporeporter/RepoReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shuoros.reporeporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoReporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
