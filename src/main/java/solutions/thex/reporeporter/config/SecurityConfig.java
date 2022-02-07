cea0d53a481613be45f1b5b727da3c5fd90fe77c
Soroush <s_shemshadi@ymail.com>
2022-02-07 10:46:03 +0330

    IMPLEMENT(security): implement spring security config. permit al requests on "/api/**" and restrict accesses to "/admin/**" and "/user/**"

---
+++ src/main/java/solutions/thex/reporeporter/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package solutions.thex.reporeporter.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UnauthorizedEntryPoint unauthorizedEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()//
+                .authorizeRequests()//
+                .antMatchers("/api/**").permitAll()//
+                .antMatchers("/admin/**").hasAnyAuthority("ADMIN")//
+                .antMatchers("/user/**").hasAnyAuthority("USER")//
+                .anyRequest().authenticated()//
+                .and()//
+                .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint)//
+                .and()//
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+}
