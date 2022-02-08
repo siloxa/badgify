6b84c14a3359f3680543cd530d5dab67ae0b3bb1
Soroush <s_shemshadi@ymail.com>
2022-02-08 21:30:56 +0330

    IMPLEMENT(ISoyConfiguration): implement an abstract for implementing configuration of SoyTemplateGenerator

---
+++ src/main/java/solutions/thex/reporeporter/soy/ISoyConfiguration.java
@@ -0,0 +1,36 @@
+package solutions.thex.reporeporter.soy;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Map;
+
+public interface ISoyConfiguration {
+
+    /**
+     * @return soy fully-qualified name
+     */
+    String getName();
+
+    /**
+     * @return template file path
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    File getFile() throws IOException;
+
+    /**
+     * @return map of soy parameters
+     */
+    Map<String, Object> getParameters();
+
+    /**
+     * @return target file path
+     */
+    Path getPath() throws IOException;
+
+    default void generate() throws IOException {
+        SoyTemplateGenerator.generate(this);
+    }
+}
