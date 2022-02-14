bd561d95386f4e00e0886819a373c9281d02baf0
Soroush <s_shemshadi@ymail.com>
2022-02-14 14:42:24 +0330

    UPDATE(BadgeController): update customBadge method to receive parameters in separate params.

--- src/main/java/solutions/thex/reporeporter/controller/BadgeController.java
+++ src/main/java/solutions/thex/reporeporter/controller/BadgeController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import solutions.thex.reporeporter.svg.DesignParser;
-import solutions.thex.reporeporter.svg.DesignResolver;
+import solutions.thex.reporeporter.svg.SvgGenerator;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -19,29 +17,94 @@ import java.io.IOException;
 public class BadgeController {
 
     /**
-     * Returns a badge for the given repository.
+     * Creates custom badges.
      *
-     * ?design={first=repo%20reporter;second=$GITHUB$;direction=h;left=second;right=first;color=#fff;bg=#f48024;
-     * theme=simple;link=https://github.com/TheXSolutions/repo-reporter}
+     * Example: /?title=Repo%20Reporter&bg=f48024&link=https://repo-reporter.thex.solutions
      *
      * @param request The request.
-     * @param design  The design.
+     * @param title   The title of the badge.
+     * @param link    The link of the badge.
+     * @param bg      The background color of the badge.
+     * @param size    The size of the badge.
+     * @param width   The width of the badge.
+     * @param height  The height of the badge.
      * @return The badge.
      */
     @GetMapping(path = "/", produces = "image/svg+xml")
-    public ResponseEntity<String> customBadge(HttpServletRequest request,
-                                              @RequestParam(value = "design", required = true, defaultValue = "-1")
-                                                      String design) throws Exception {
+    public ResponseEntity<String> customBadge(HttpServletRequest request,//
+                                              @RequestParam(value = "title")//
+                                                      String title,
+                                              @RequestParam(value = "theme", required = false, defaultValue = "simple")//
+                                                      String theme,//
+                                              @RequestParam(value = "size", required = false, defaultValue = "m")//
+                                                      String size,//
+                                              @RequestParam(value = "direction", required = false, defaultValue = "ltr")//
+                                                      String direction,//
+                                              @RequestParam(value = "link", required = false, defaultValue = "#")//
+                                                      String link,//
+                                              @RequestParam(value = "width", required = false, defaultValue = "-1")//
+                                                      String width,
+                                              @RequestParam(value = "height", required = false, defaultValue = "-1")//
+                                                      String height,//
+                                              @RequestParam(value = "bg", required = false, defaultValue = "#e1e2e8")//
+                                                      String bg,//
+                                              @RequestParam(value = "color", required = false, defaultValue = "rgb(255, 255, 255)")//
+                                                      String color) throws Exception {
         log.info("BadgeController.customBadge: " //
-                + "payload: " + design//
-                + "path= " + request.getRequestURI()//
+                + "payload: [" + theme//
+                + ", " + size//
+                + ", " + direction//
+                + ", " + title//
+                + ", " + link//
+                + ",  " + width//
+                + ", " + height//
+                + ", " + bg//
+                + ", " + color + "]"//
+                + ", path= " + request.getRequestURI()//
                 + ", ip= " + request.getRemoteAddr()//
                 + ", user agent= " + request.getHeader("User-Agent"));
 
-        if ("-1".equals(design))
-            return new ResponseEntity<>("design can not be empty", HttpStatus.BAD_REQUEST);
+        String svg = SvgGenerator.builder()//
+                .style("badge")//
+                .theme(theme)//
+                .size(size)//
+                .direction(direction)//
+                .title(title)//
+                .link(link)//
+                .width(resolveWidth(width, size, title))//
+                .height(resolveHeight(height, size))//
+                .bg(bg)//
+                .color(color)//
+                .build().render();
+        return new ResponseEntity<>(svg, HttpStatus.OK);
+    }
 
-        return DesignResolver.resolve(DesignParser.parse(design));
+    private static String resolveWidth(String width, String size, String title) {
+        if ("-1".equals(width))
+            return extractWidthDefaultValue(size, title);
+        return width;
+    }
+
+    private static String resolveHeight(String height, String size) {
+        if ("-1".equals(height))
+            return extractHeightDefaultValue(size);
+        return height;
+    }
+
+    private static String extractWidthDefaultValue(String size, String title) {
+        String defaultWidth = "";
+        if (size.equals("m")) {
+            defaultWidth = String.valueOf((int) Math.ceil((title.length() * 6.4117647) + 29));
+        }
+        return defaultWidth;
+    }
+
+    private static String extractHeightDefaultValue(String size) {
+        String defaultHeight = "";
+        if (size.equals("m")) {
+            defaultHeight = "24";
+        }
+        return defaultHeight;
     }
 
 }
