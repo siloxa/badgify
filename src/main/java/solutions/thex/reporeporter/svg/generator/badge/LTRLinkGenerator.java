17787ff228d845824697db19f937d892d66e1e42
Soroush <s_shemshadi@ymail.com>
2022-02-23 22:22:22 +0330

    IMPLEMENT(RTL & LTR LinkGenerator): implement SvgGenerator abstract class's getParameters and getTemplatePath methods to generate link badges.

---
+++ src/main/java/solutions/thex/reporeporter/svg/generator/badge/LTRLinkGenerator.java
@@ -0,0 +1,50 @@
+package solutions.thex.reporeporter.svg.generator.badge;
+
+import lombok.Builder;
+import solutions.thex.reporeporter.svg.SvgGenerator;
+
+import java.util.Map;
+
+@Builder
+public class LTRLinkGenerator extends SvgGenerator {
+
+    private String theme;
+    private String size;
+    private String title;
+    private String logo;
+    private String link;
+    private String textLength;
+    private String titleXPosition;
+    private String width;
+    private String height;
+    private String bg;
+    private String color;
+
+    @Override
+    public Map<String, Object> getParameters() {
+        System.out.println("title"+ title+ //
+                "link"+ link+ //
+                "textLength"+ textLength+ //
+                "titleXPosition"+ titleXPosition+ //
+                "width"+ width+ //
+                "height"+ height+ //
+                "bg"+ bg+ //
+                "color"+ color);
+        return Map.of(//
+                "title", title, //
+                "logo", logo, //
+                "link", link, //
+                "textLength", textLength, //
+                "titleXPosition", titleXPosition, //
+                "width", width, //
+                "height", height, //
+                "bg", bg, //
+                "color", color);
+    }
+
+    @Override
+    public String getTemplatePath() {
+        return "templates/link/" + theme + "-" + size + "-ltr.svg.soy";
+    }
+
+}
