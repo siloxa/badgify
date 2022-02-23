1c817ff7b5888c190e784a6eb003061bd72126f4
Soroush <s_shemshadi@ymail.com>
2022-02-23 22:14:08 +0330

    ADD(time zone configuration): set application's time zone in GMT

--- src/main/java/solutions/thex/reporeporter/RepoReporterApplication.java
+++ src/main/java/solutions/thex/reporeporter/RepoReporterApplication.java
@@ -3,11 +3,20 @@ package solutions.thex.reporeporter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class RepoReporterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RepoReporterApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
     }
 
 }
