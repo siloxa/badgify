b2862a088912b259a7df2af6b6dcc234848bda36
Soroush <s_shemshadi@ymail.com>
2022-02-24 17:37:59 +0330

    IMPLEMENT(RepoReporterApplicationTests): implement application unit tests.

--- src/test/java/solutions/thex/reporeporter/RepoReporterApplicationTests.java
+++ src/test/java/solutions/thex/reporeporter/RepoReporterApplicationTests.java
@@ -1,13 +1,29 @@
 package solutions.thex.reporeporter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import solutions.thex.reporeporter.controller.BadgeController;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class RepoReporterApplicationTests {
 
+    @Autowired
+    private RepoReporterApplication application;
+
+    @Autowired
+    private BadgeController badgeController;
+
     @Test
     void contextLoads() {
+        assertNotNull(application);
+    }
+
+    @Test
+    void badgeControllerLoads() {
+        assertNotNull(badgeController);
     }
 
 }
