58955cc09438f458404845e412a97d5d169bd317
Soroush <s_shemshadi@ymail.com>
2022-02-28 15:38:01 +0330

    IMPLEMENT(InlineSvgAsResponseWrapper): implement an interface for Inline requests which wraps generated SVG from user string params(design) As ResponseEntity.

---
+++ src/main/java/solutions/thex/reporeporter/svg/InlineSvgAsResponseWrapper.java
@@ -0,0 +1,18 @@
+package solutions.thex.reporeporter.svg;
+
+import org.springframework.http.ResponseEntity;
+import solutions.thex.reporeporter.util.Regex;
+
+import java.io.IOException;
+
+public interface InlineSvgAsResponseWrapper {
+
+    ResponseEntity<String> wrap(String design) throws IOException;
+
+    default Boolean isDesignValid(String design) {
+        return Regex.builder()//
+                .pattern("[a-zA-Z0-9+#+%]\\w*")//
+                .build().matches(design);
+    }
+
+}
