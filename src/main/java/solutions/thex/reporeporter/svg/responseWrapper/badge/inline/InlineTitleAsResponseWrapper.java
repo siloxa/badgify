5fcf65e9e533f956dca95ba4c377bf98d9559367
Soroush <s_shemshadi@ymail.com>
2022-02-28 15:43:02 +0330

    IMPLEMENT(InlineTitleAsResponseWrapper): implement InlineSvgAsResponseWrapper interface's wrap method to warp generated SVGs as ResponseEntity.

---
+++ src/main/java/solutions/thex/reporeporter/svg/responseWrapper/badge/inline/InlineTitleAsResponseWrapper.java
@@ -0,0 +1,80 @@
+package solutions.thex.reporeporter.svg.responseWrapper.badge.inline;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import solutions.thex.reporeporter.controller.error.ErrorAsBadge;
+import solutions.thex.reporeporter.svg.InlineSvgAsResponseWrapper;
+import solutions.thex.reporeporter.svg.resolver.badge.TitleResolver;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InlineTitleAsResponseWrapper implements InlineSvgAsResponseWrapper {
+
+    @Override
+    public ResponseEntity<String> wrap(String design) throws IOException {
+        if (isDesignValid(design)) {
+            final Map<String, String> params =//
+                    extractDesign(design);
+            if (params.size() == 0) {
+                return new ResponseEntity<>(//
+                        new ErrorAsBadge(422, "Title not provided!").toString(),//
+                        HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+            return new ResponseEntity<>(//
+                    new TitleResolver().resolve(params) //
+                    , HttpStatus.OK);
+        }
+        return new ResponseEntity<>(//
+                new ErrorAsBadge(422, "Design syntax error!").toString(),//
+                HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    public ResponseEntity<String> wrapShort(String design) throws IOException {
+        if (isDesignValid(design)) {
+            final Map<String, String> params =//
+                    extractShortDesign(design);
+            if (params.size() == 3) {
+                return new ResponseEntity<>(//
+                        new ErrorAsBadge(422, "Title not provided!").toString(),//
+                        HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+            return new ResponseEntity<>(//
+                    new TitleResolver().resolve(params) //
+                    , HttpStatus.OK);
+        }
+        return new ResponseEntity<>(//
+                new ErrorAsBadge(422, "Design syntax error!").toString(),//
+                HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    private Map<String, String> extractShortDesign(String design) {
+        Map<String, String> params = new HashMap<>();
+        String[] designParts = design.split("-");
+        if (designParts.length == 3) {
+            params.put("title", designParts[0]);
+            params.put("bg", designParts[1]);
+            params.put("color", designParts[2]);
+        }
+        params.put("size", "s");
+        params.put("theme", "simple");
+        params.put("link", "#");
+        return params;
+    }
+
+    private Map<String, String> extractDesign(String design) {
+        Map<String, String> params = new HashMap<>();
+        String[] designParts = design.split("-");
+        if (designParts.length == 6) {
+            params.put("size", designParts[0]);
+            params.put("theme", designParts[1]);
+            params.put("title", designParts[2]);
+            params.put("bg", designParts[3]);
+            params.put("color", designParts[4]);
+            params.put("link", designParts[5]);
+        }
+        return params;
+    }
+
+}
