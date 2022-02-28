23f161535590ae4e9d5e7cdb6194d1382909c5b3
Soroush <s_shemshadi@ymail.com>
2022-02-28 15:40:36 +0330

    IMPLEMENT(Regex): a class to apply a regex to user's params String to validate it as : link-direction-size-theme-title-logo-bg-color-link(this example is for Link badges).

---
+++ src/main/java/solutions/thex/reporeporter/util/Regex.java
@@ -0,0 +1,22 @@
+package solutions.thex.reporeporter.util;
+
+import lombok.Builder;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Builder
+public class Regex {
+
+    private String pattern;
+
+    public boolean matches(final String str) {
+        // Compile regular expression
+        final Pattern pattern = Pattern.compile(this.pattern);
+        // Match regex against str
+        final Matcher matcher = pattern.matcher(str);
+        // Use results...
+        return matcher.find();
+    }
+
+}
