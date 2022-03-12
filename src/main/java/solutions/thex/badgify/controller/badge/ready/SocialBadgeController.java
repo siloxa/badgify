39b2f78845ff3b62c3209335d465ca4943f84d52
Soroush <s_shemshadi@ymail.com>
2022-03-12 13:03:34 +0330

    ADD(SocialBadgeController): add a controller for ready to use social networks badges controller.

---
+++ src/main/java/solutions/thex/badgify/controller/badge/ready/SocialBadgeController.java
@@ -0,0 +1,9 @@
+package solutions.thex.badgify.controller.badge.ready;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/social")
+public class SocialBadgeController {
+}
