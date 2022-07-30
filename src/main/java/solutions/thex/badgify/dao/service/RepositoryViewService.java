345d05a419ecd8264e1c3badd3d3d2ce0b65aba6
Soroush <s_shemshadi@ymail.com>
2022-07-30 10:18:17 +0430

    UPDATE(RepositoryViewService) Remove redundant import.

--- src/main/java/solutions/thex/badgify/dao/service/RepositoryViewService.java
+++ src/main/java/solutions/thex/badgify/dao/service/RepositoryViewService.java
@@ -2,7 +2,6 @@ package solutions.thex.badgify.dao.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import solutions.thex.badgify.dao.ProfileView;
 import solutions.thex.badgify.dao.RepositoryView;
 import solutions.thex.badgify.dao.repository.RepositoryViewRepository;
 
