d976a14552ed8693bb94eeb58e995042dc404178
shynmah <125250436+shynmah@users.noreply.github.com>
2024-08-04 21:02:03 +0330

    build header and footer component

--- FrontEnd/Badgify/src/App.jsx
+++ FrontEnd/Badgify/src/App.jsx
@@ -1,29 +1,22 @@
-
-import Layout from "./components/layout/Layout"
-import {BrowserRouter,Route,Routes} from "react-router-dom"
-import Home from "./components/template/Home"
-import Donate from "./components/template/Donate"
-import Developer from "./components/template/Developer"
-import Opensource from "./components/template/Opensource"
+import Layout from './components/layout/Layout';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/template/Home';
+import Donate from './components/template/Donate';
+import Developer from './components/template/Developer';
+import Opensource from './components/template/Opensource';
 function App() {
   return (
     <Layout>
- <BrowserRouter>
-      <Routes>
-        
-        <Route path="/home" element={<Home/>} />
-        <Route path="/donate" element={<Donate/>} />
-        <Route path="/developer" element={<Developer/>} />
-        <Route path="/opensource" element={<Opensource/>} />
-        
-        
-
-        
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/developer" element={<Developer />} />
+          <Route path="/opensource" element={<Opensource />} />
+        </Routes>
+      </BrowserRouter>
     </Layout>
-   
-  )
+  );
 }
 
-export default App
+export default App;
--- FrontEnd/Badgify/src/components/layout/Footer.jsx
+++ FrontEnd/Badgify/src/components/layout/Footer.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styles from './footer.module.css';
 
 function Footer() {
   return (
-    <div>
-      footer
-    </div>
-  )
+    <footer>
+      <div className={styles.container}>
+        <div className={styles.footer}>
+          <div className={styles.footerLeft}>
+            <p>© Licensed under <a>MIT</a></p>
+          </div>
+          <div className={styles.footerRight}>
+            <ul>
+              <li>Donate</li>
+              <li>Open source</li>
+              <li>Developers</li>
+              <li>Any issues?</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
