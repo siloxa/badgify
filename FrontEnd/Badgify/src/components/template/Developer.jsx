a70835b33abe2f2535b3b6bda94789755234d1ce
shynmah <125250436+shynmah@users.noreply.github.com>
2024-08-04 21:45:16 +0330

    fixed some css in footer and global

--- FrontEnd/Badgify/src/components/template/Developer.jsx
+++ FrontEnd/Badgify/src/components/template/Developer.jsx
@@ -1,20 +1,18 @@
-
-import DevCard from '../module/DevCard'
-import { devData } from '../../data/DeveloperData'
-import style from "../template/Developer.module.css"
+import DevCard from '../module/DevCard';
+import { devData } from '../../data/DeveloperData';
+import style from '../template/Developer.module.css';
 function Developer() {
-  console.log(devData)
+  console.log(devData);
   return (
     <>
-            <h1>Meet out team</h1>
+      <h1>Meet out team</h1>
       <div className={style.container}>
-
-        {devData.map((i) => (
-      <DevCard key={i.id} data={i} />
-    ))}
+        {devData.map(i => (
+          <DevCard key={i.id} data={i} />
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default Developer
+export default Developer;
