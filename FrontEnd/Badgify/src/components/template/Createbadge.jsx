11891069ccbcf63a449891fd3480b08c92fcbd6d
shynmah <125250436+shynmah@users.noreply.github.com>
2024-08-20 13:00:41 +0330

    fixed select option with ternery in costomize page

--- FrontEnd/Badgify/src/components/template/Createbadge.jsx
+++ FrontEnd/Badgify/src/components/template/Createbadge.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './createbadge.module.css';
 function Createbadge() {
+  const [selectedItem, setSelectedItem] = useState('Lable badge');
+  const getSelectedHandler = event => {
+    setSelectedItem(event.target.value);
+  };
   return (
     <div className={styles.container}>
       <form className={styles.form}>
         <div className={styles.inputcon}>
           <div className={styles.inputmain}>
             <label>Type</label>
-            <select>
-              <option>Lable badge</option>
-              <option>Text badge</option>
-              <option>Icon badge</option>
-              <option></option>
+            <select name="Type" onChange={getSelectedHandler} value={selectedItem}>
+              <option value="Lable badge">Lable badge</option>
+              <option value="Text badge">Text badge</option>
+              <option value="Icon badge">Icon badge</option>
             </select>
           </div>
           <div className={styles.inputmain}>
@@ -19,26 +22,122 @@ function Createbadge() {
             <select></select>
           </div>
         </div>
-        <div className={styles.inputcon}>
-          <div className={styles.inputmain}>
-            <label>Size</label>
-            <input className={styles.inputs} placeholder="Type hex code or color’s name" />
-          </div>
-          <div className={styles.inputmain}>
-            <label>Title</label>
-            <input className={styles.inputs} placeholder="Type your ID" />
-          </div>
-        </div>
-        <div className={styles.inputcon}>
-          <div className={styles.inputmain}>
-            <label>Background Color</label>
-            <input className={styles.inputs} placeholder="Type hex code or color’s name" />
-          </div>
-          <div className={styles.inputmain}>
-            <label>Text Color</label>
-            <input className={styles.inputs} placeholder="Type your ID" />
-          </div>
-        </div>
+        {selectedItem === 'Lable badge' && (
+          <>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Size</label>
+                <input className={styles.inputs} placeholder="Type hex code or color’s name" />
+              </div>
+              <div className={styles.inputmain}>
+                <label>Title</label>
+                <input className={styles.inputs} placeholder="Type your ID" />
+              </div>
+            </div>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Background Color</label>
+                <input className={styles.inputs} placeholder="Type hex code or color’s name" />
+              </div>
+              <div className={styles.inputmain}>
+                <label>Text Color</label>
+                <input className={styles.inputs} placeholder="Type your ID" />
+              </div>
+            </div>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Theme</label>
+                <select></select>
+              </div>
+              <div className={styles.inputmain}>
+                <label>Text Color</label>
+                <input className={styles.inputs} placeholder="Type your ID" />
+              </div>
+            </div>
+          </>
+        )}
+        {selectedItem === 'Text badge' && (
+          <>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Type</label>
+                <select name="Type">
+                  <option value="Lable badge">Lable badge</option>
+                  <option value="Text badge">Text badge</option>
+                  <option value="Icon badge">Icon badge</option>
+                </select>
+              </div>
+              <div className={styles.inputmain}>
+                <label>Theme</label>
+                <select></select>
+              </div>
+            </div>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Background Color</label>
+                <input className={styles.inputs} placeholder="Type hex code or color’s name" />
+              </div>
+              <div className={styles.inputmain}>
+                <label>Text Color</label>
+                <input className={styles.inputs} placeholder="Type your ID" />
+              </div>
+            </div>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Size</label>
+                <select></select>
+              </div>
+              <div className={styles.inputmain}>
+                <label>Link</label>
+                <input className={styles.inputs} placeholder="Type your ID" />
+              </div>
+            </div>
+          </>
+        )}
+        {selectedItem === 'Icon badge' && (
+          <>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Type</label>
+                <select name="Type" onChange={getSelectedHandler} value={selectedItem}>
+                  <option value="Lable badge">Lable badge</option>
+                  <option value="Text badge">Text badge</option>
+                  <option value="Icon badge">Icon badge</option>
+                </select>
+              </div>
+              <div className={styles.inputmain}>
+                <label>Theme</label>
+                <select></select>
+              </div>
+            </div>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Theme</label>
+                <select></select>
+              </div>
+              <div className={styles.inputmain}>
+                <label>Theme</label>
+                <select></select>
+              </div>
+            </div>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Background Color</label>
+                <input className={styles.inputs} placeholder="Type hex code or color’s name" />
+              </div>
+              <div className={styles.inputmain}>
+                <label>Text Color</label>
+                <input className={styles.inputs} placeholder="Type your ID" />
+              </div>
+            </div>
+            <div className={styles.inputcon}>
+              <div className={styles.inputmain}>
+                <label>Text Color</label>
+                <input className={styles.inputs} placeholder="Type your ID" />
+              </div>
+            </div>
+          </>
+        )}
       </form>
       <div className={styles.inputBot}>
         <div className={styles.bot}>
